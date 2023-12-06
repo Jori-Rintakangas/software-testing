79d0f421b1b15ad25ed4f433a12b1e11c8a7f3b8
Jori Rintakangas <jori.rintakangas@tuni.fi>
2023-12-06 13:52:10 +0200

    More toString tests

--- test/toString.test.js
+++ test/toString.test.js
@@ -3,12 +3,28 @@ import toString from "../src/toString.js"
 const expect = chai.expect
 
 describe("toString", () => {
- it("convert integer to string", () =>{
+ it("convert an integer to string", () =>{
     expect(toString(3)).to.equal("3")
  });
 
- it("convert string to string", () =>{
+ it("convert a string to string", () =>{
    expect(toString("This is a string")).to.equal("This is a string")
+ });
+
+ it("convert a float to string", () =>{
+  expect(toString(3.14)).to.equal("3.14")
+ });
+
+ it("convert an array to string", () =>{
+  expect(toString([1, 2, null, 3])).to.equal("1,2,,3")
+ });
+
+ it("convert a symbol to string", () =>{
+  expect(toString(Symbol("test symbol"))).to.equal("Symbol(test symbol)")
+ });
+
+ it("convert minus zero to string", () =>{
+  expect(toString(-0)).to.equal("-0")
  });
 
 })
