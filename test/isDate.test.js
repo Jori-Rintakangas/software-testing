f226de9a1d3dcd7da8201a3b94db5a7edecf8fbb
Jori Rintakangas <jori.rintakangas@tuni.fi>
2023-12-07 10:38:43 +0200

    isDate tests added

---
+++ test/isDate.test.js
@@ -0,0 +1,26 @@
+import chai from "chai"
+import isDate from "../src/isDate.js"
+const expect = chai.expect
+
+describe("isDate", () => {
+ it("check that a Date object is a date", () =>{
+    expect(isDate(new Date("October 13, 2014 11:13:00"))).to.be.true
+ });
+
+ it("check that string date is not a date", () =>{
+    expect(isDate("Mon April 23 2012")).to.be.false
+ });
+
+ it("check that null is not a date", () =>{
+    expect(isDate(null)).to.be.false
+ });
+
+ it("check that undefined is not a date", () =>{
+    expect(isDate(undefined)).to.be.false
+ });
+
+ it("check that an empty object is not a date", () =>{
+    expect(isDate({})).to.be.false
+ });
+
+})
