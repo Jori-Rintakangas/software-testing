adc192d394273985c9bda343b80f7795df488646
Jori Rintakangas <jori.rintakangas@tuni.fi>
2023-12-07 12:08:32 +0200

    Added upperFirst tests

---
+++ test/upperFirst.test.js
@@ -0,0 +1,22 @@
+import chai from "chai"
+import upperFirst from "../src/upperFirst.js";
+const expect = chai.expect
+
+describe("upperFirst", () => {
+ it("convert an upper case to upper case", () =>{
+    expect(upperFirst("Software testing")).to.equal("Software testing")
+ });
+
+ it("convert a lower case to upper case", () =>{
+    expect(upperFirst("software testing")).to.equal("Software testing")
+ });
+
+ it("convert a single character to upper case", () =>{
+    expect(upperFirst("a")).to.equal("A")
+ });
+
+ it("convert a string with unicode characters to upper case", () =>{
+    expect(upperFirst("this is a smiley face \ud83d\ude00\u0000")).to.equal("This is a smiley face \ud83d\ude00\u0000")
+ });
+
+})
